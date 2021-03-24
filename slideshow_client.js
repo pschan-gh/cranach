946567985abfb2b8f12d514ad46e81615bd07c20
Ping-Shun Chan <31397850+pschan-gh@users.noreply.github.com>
2021-03-24 10:38:16 +0800

    update

--- slideshow_client.js
+++ slideshow_client.js
@@ -212,7 +212,8 @@ function print(promise) {
     });
     $('#print_content').find('.collapsea').hide();
     $('#print_content').find('.collapse').show();
-
+    $('#print_content').find('.hidden_collapse').show();
+    
 }
 
 function removeTypeset() { // i.e. Show LaTeX source
