7fe2c41906d30c2e4902a2e46f82f0de995057fe
pschan-gh <31397850+pschan-gh@users.noreply.github.com>
2022-01-04 19:33:58 +0800

    lcref now uses bootstrap

--- js/lcref.js
+++ js/lcref.js
@@ -1,7 +1,7 @@
 /*
-* lcref - Feature Demo for lcrefs
+* Lcref
 *
-* Based primarily on the work on Knowl by:
+* Derived from the work on Knowl by:
 *
 * Copyright (C) 2011  Harald Schilly
 *
@@ -19,13 +19,9 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/* javascript code for the lcref features
-* global counter, used to uniquely identify each lcref-output element
-* that's necessary because the same lcref could be referenced several times
-* on the same page */
+/* Requires Bootstrap v5 */
 
 let lcref_id_counter = 0;
-
 const lcref_focus_stack_uid = [];
 const lcref_focus_stack = [];
 
@@ -33,60 +29,43 @@ function lcref_click_handler(el) {
 	let lcref = el.getAttribute("lcref");
 	let uid = el.getAttribute("lcref-uid");
 	let output_id = 'lcref-output-' + uid;
-
+    
 
 	let lcrefid = 'kuid-' + uid;
+    
+    el.dataset.bsToggle = 'collapse';
+    el.setAttribute('href', `#${lcrefid}`);
+    
 	const lcrefContainer = document.querySelector(`#${lcrefid}`);
 	if (lcrefContainer !== null) {
-		if (lcrefContainer.classList.contains('hidden')) {
-			expandLcref(lcrefContainer);
-		} else {
-			lcrefContainer.style.height = '0px';
-			lcrefContainer.classList.add('hidden');
-		}
-		// lcrefContainer.style.height = '0px';
-		// document.querySelector(`#${lcrefid}`).slideToggle("fast", function() {
-		// 	if ($('#' + lcrefid).is(":visible")) {
-		// 		document.getElementById(lcrefid).scrollIntoView();
-		// 	} else {
-		// 		el.scrollIntoView();
-		// 	}
-		// });
+        // lcrefContainer.querySelector('.icon.loading').classList.add('hidden');
+        bootstrap.Collapse.getOrCreateInstance(lcrefContainer).toggle();
 		if (document.getElementById(lcrefid).offsetParent !== null) {
 			document.getElementById(lcrefid).scrollIntoView();
 		} else {
 			el.scrollIntoView();
 		}
-
-		this_lcref_focus_stack_uidindex = lcref_focus_stack_uid.indexOf(uid);
-
-		if(el.classList.contains("active")) {
-			if(this_lcref_focus_stack_uidindex != -1) {
-				lcref_focus_stack_uid.splice(this_lcref_focus_stack_uidindex, 1);
-				lcref_focus_stack.splice(this_lcref_focus_stack_uidindex, 1);
-			}
-		}
-		else {
-			lcref_focus_stack_uid.push(uid);
-			lcref_focus_stack.push(el);
-			document.getElementById(lcrefid).scrollIntoView();
-		}
-
 	} else {
 		const lcrefContainer = document.createElement('div');
 		lcrefContainer.id = lcrefid;
-		lcrefContainer.classList.add('lcref-output', 'hidden');
+		lcrefContainer.classList.add('lcref-output', 'collapse');
 		lcrefContainer.innerHTML = `<div class="lcref">`
-		+ `<div class="lcref-content" id="${output_id}">loading ${lcref}</div>`
+		+ `<div class="spinner-border icon loading" role="status" style="display:block; margin:30px auto; opacity:0.25; width: 75px; height: 75px;">`
+        + `<span class="visually-hidden">Loading...</span>`
+        + `</div>`
+        + `<div class="lcref-content hidden" style="overflow-y:hidden" id="${output_id}">`
+        + `</div>`
 		+ `<div class='lcref-footer'>${lcref}</div>`
 		+ `</div></div>`;
-		lcrefContainer.style.transition = 'height 0.35s ease-in-out';
+        lcrefContainer.style.transition = 'height 0.5s ease-in-out';
 
 		if (el.closest('.paragraphs') !== null) {
 			el.closest('.paragraphs').after(lcrefContainer);
 		} else {
 			el.after(lcrefContainer);
 		}
+        bootstrap.Collapse.getOrCreateInstance(lcrefContainer).toggle();
+        
 		const lcrefOutput = lcrefContainer.querySelector('.lcref-content');
 		const url = el.getAttribute('lcref');
 		const params = url.match(/\?(.*?)(#|$)/);
@@ -109,12 +88,6 @@ function lcref_click_handler(el) {
 			});
 		}
 
-		// $lcrefContainer.slideDown("slow", function() {
-		// 	if ($('.carousel-item').length > 0) {
-		// 		adjustHeight();
-		// 		document.getElementById(lcrefid).scrollIntoView();
-		// 	}
-		// });
 		if (document.querySelector('.carousel-item') !== null) {
 			adjustHeight();
 			document.getElementById(lcrefid).scrollIntoView();
@@ -126,7 +99,7 @@ function lcref_click_handler(el) {
 		lcref_focus_stack.push(el);
 		document.querySelectorAll("a[lcref]").forEach(a => a.setAttribute("href", ""));
 	}
-} //~~ end click handler for *[lcref] elements
+}
 
 function renderElement(lcrefContainer) {
 	lcrefContainer.querySelectorAll('img').forEach(el => imagePostprocess(el));
@@ -140,34 +113,10 @@ function renderElement(lcrefContainer) {
 	if (document.querySelector('#output').classList.contains('present')) {
 		updateCarouselSlide(document.querySelector('#output > div.slide.active'));
 	}
-	expandLcref(lcrefContainer);
+	// toggleLcref(lcrefContainer);
+    MathJax.startup.promise.then(() => {
+        lcrefContainer.querySelector('.icon.loading').classList.add('hidden');
+        lcrefContainer.querySelector('.lcref-content').classList.remove('hidden');
+    });
+    
 }
-
-function expandLcref(lcrefContainer) {
-	MathJax.startup.promise.then(() => {
-		lcrefContainer.style.height = 'auto';
-		lcrefContainer.classList.remove('hidden');
-		const clientHeight = lcrefContainer.clientHeight;
-		lcrefContainer.style.height = '0px';
-
-		setTimeout(function () {
-			lcrefContainer.style.height = clientHeight + 'px';
-		}, 0);
-	});
-}
-
-// document.addEventListener('DOMContentLoaded', () => {
-// 	document.querySelectorAll("body [lcref]").forEach(el => {
-// 		el.addEventListener('click', function(evt) {
-// 			evt.preventDefault();
-// 			evt.stopPropagation();
-// 			const lcref = evt.target;
-// 			if(!lcref.hasAttribute("lcref-uid")) {
-// 				lcref.setAttribute("lcref-uid", lcref_id_counter);
-// 				lcref_id_counter++;
-// 			}
-// 			lcref_click_handler(lcref, evt);
-// 		});
-// 		el.setAttribute("href", "");
-// 	});
-// });
